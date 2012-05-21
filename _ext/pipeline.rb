931f5b1adaf2a91187ac1b09f37711994addc2c1
Aslak Knutsen <aslak@redhat.com>
2012-05-21 12:29:39 +0200

    Add prettify to blogs posts and clean up

---
+++ _ext/pipeline.rb
@@ -0,0 +1,8 @@
+require 'gist'
+
+Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::Gist.new( 'aslakknutsen' ) 
+  extension Awestruct::Extensions::Posts.new( '/blog' ) 
+  # extension Awestruct::Extensions::Indexifier.new
+end
+
